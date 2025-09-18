6e120c1f19feaffbc69f834540a6165c203c27ae
Tejas ///M <tejasm9380@gmail.com>
2025-09-18 15:58:16 +0530

    Delete DSA directory

--- DSA/leetcode/mejority elements.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        candidate = nums[0]
-        score = 1
-        for num in nums[1:]:
-            if score == 0:
-                candidate = num
-            score += 1 if num == candidate else -1
-        return candidate
